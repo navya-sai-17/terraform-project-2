070a76945bfd1cd4c8663cdca616e18468a1690a
navya-sai-17 <navyasaidanaboina17@gmail.com>
2025-07-17 05:13:25 +0000

    second-commit

--- resource.tf
+++ resource.tf
@@ -2,8 +2,3 @@ resource "azurerm_resource_group" "terraazure" {
   name     = "terraazure"
   location = "East US"
 }
-
-resource "azurerm_resource_group" "terra" {
-  name     = "terra"
-  location = "East US"
-}
